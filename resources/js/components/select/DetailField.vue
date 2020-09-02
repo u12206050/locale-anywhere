799fdf7da44032d19080a22de5f8c820a43bef91
Gerard Lamusse <>
2020-09-02 17:48:04 +0200

    Fixed Language Select Detail View

--- resources/js/components/select/DetailField.vue
+++ resources/js/components/select/DetailField.vue
@@ -1,8 +1,12 @@
 <template>
-  <span>
-    <span class="flag-icon mr-2" :style="flag(field.value)"></span>
-    {{ field.locales[field.value] }}
-  </span>
+  <panel-item :field="field">
+      <template slot="value" v-if="field.value">
+        <span>
+          <span class="flag-icon mr-2" :style="flag(field.value)"></span>
+          {{ field.locales[field.value] }}
+        </span>
+      </template>
+  </panel-item>
 </template>
 
 <script>
